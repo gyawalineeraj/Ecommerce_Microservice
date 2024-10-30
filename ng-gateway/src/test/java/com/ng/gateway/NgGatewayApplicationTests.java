8b360357b4ca9342ebf22c53788de19af9e0f836
Neeraj Gyawali <gyawalineeraj777@gmail.com>
2024-10-30 16:31:40 +0545

    step 2

---
+++ ng-gateway/src/test/java/com/ng/gateway/NgGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ng.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
